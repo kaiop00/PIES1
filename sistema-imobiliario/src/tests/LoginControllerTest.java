e040ae8abb8b6d223b82abc431e0f562f70ac936
Lucas Bertoldo <lucas.fber@gmail.com>
2022-10-26 19:06:38 -0300

    projeto criado, primeiro test JUnit exemplo

---
+++ sistema-imobiliario/src/tests/LoginControllerTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+
+	@Test
+	void test() {
+		Assert.assertEquals(0, 0);
+	}
+
+}
