c0eee2a042aff0c622ef2d5d1cf6cbc57fb7c096
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2022-11-09 11:10:15 -0300

    criado os testes da classe Client

--- sistema-imobiliario/src/tests/ClientTest.java
+++ sistema-imobiliario/src/tests/ClientTest.java
@@ -19,9 +19,64 @@ class ClientTest {
 		assertEquals("Jose",client.getName());
 	}
 	
+	@Test
 	void testGetCPF() {
 		Client client = new Client("Jose","000000000","email@email.com","00000","00000");
 		assertEquals("000000000",client.getCpf());
 	}
+	
+	@Test
+	void testGetEmail() {
+		Client client = new Client("Jose","000000000","email@email.com","00000","00000");
+		assertEquals("email@email.com",client.getEmail());
+	}
+	
+	@Test
+	void testGetAddress() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		assertEquals("Rua X, 321", client.getAddress());
+	}
+	
+	@Test
+	void testGetPhone() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		assertEquals("0000",client.getPhone());
+	}
+	
+	@Test
+	void testSetName() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		client.setName("Joao");
+		assertEquals("Joao",client.getName()); 
+	}
+	
+	@Test
+	void testSetCpf() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		client.setCpf("11111111111");
+		assertEquals("11111111111",client.getCpf()); 
+	}
+	
+	@Test
+	void testSetEmail() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		client.setEmail("email@email.com");
+		assertEquals("email@email.com",client.getEmail()); 
+	}
+	
+	@Test
+	void testSetAddress() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		client.setAdress("Av Maria, 1000");
+		assertEquals("Av Maria,1000",client.getAddress()); 
+	}
+	
+	@Test
+	void testSetPhone() {
+		Client client = new Client("Jose","000000000","email@email.com","Rua X, 321","0000");
+		client.setPhone("1111111111");
+		assertEquals("1111111111",client.getPhone()); 
+	}
+
 
 }
