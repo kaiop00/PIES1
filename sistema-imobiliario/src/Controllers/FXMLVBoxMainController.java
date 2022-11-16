fb2e17316a5317cb15befab09af0a8afaa868e03
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2022-11-16 10:46:42 -0300

    atualizando projeto UI

--- sistema-imobiliario/src/Controllers/FXMLVBoxMainController.java
+++ sistema-imobiliario/src/Controllers/FXMLVBoxMainController.java
@@ -1,8 +1,10 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
@@ -20,12 +22,18 @@ public class FXMLVBoxMainController implements Initializable{
 	@FXML
 	private MenuItem menuItemExluitCliente;
 	@FXML
-	private AnchorPane archorPane;
+	private AnchorPane anchorPane;
 	
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@FXML
+	public void handleMenuItemCadastrosClientes() throws IOException {
+		AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("src/ViewSystem/FMXLAnchorPaneCadastroCliete.fxml"));
+		anchorPane.getChildren().setAll(a);
 	}
 }
